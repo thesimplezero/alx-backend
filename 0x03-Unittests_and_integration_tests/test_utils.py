4b4e4a58522a6c895c13ebff9d85f44247d20167
thesimplezero <hi@thesimplezero.com>
2024-02-05 17:45:01 +0300

    add test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,13 +5,15 @@ from parameterized import parameterized
 from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Class that tests if the access_nested_map function works"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": {"c": 4}}}, ["a", "b", "c"], 4),
+        ({"a": {"b": {"c": 4}}}, ["a", "b", "d"], None),
+        ({"a": {"b": {"c": 4}}}, ["x", "y", "z"], None)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """Tests if the function returns the correct value"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == '__main__':
